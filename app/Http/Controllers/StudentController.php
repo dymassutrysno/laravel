a1d2be1e2f3c83eec4f2e19f401544dffb781115
dymassutrysno <dymassutrisno32@gmail.com>
2024-06-04 13:22:14 +0700

    praktikum 11

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Student;
+use Illuminate\Auth\Events\Validated;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
@@ -16,5 +17,75 @@ class StudentController extends Controller
         return view('admin.contents.student.index', [
             'students' => $students,
         ]);
+    }
+
+    //method untuk menampilkan tambah form
+    public function create(){
+        return view('admin.contents.student.create');
+    }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'nim' => 'required|numeric',
+            'major' => 'required',
+            'class' => 'required',
+            ]);
+
+        //simpan data ke database
+            Student::create([
+                'name' => $request->name,
+                'nim' => $request->nim,
+                'major' => $request->major,
+                'class' => $request->class,
+            ]);
+
+            return redirect('admin/student')->with('message', 'Data student berhasil ditambahkan!');
+    }
+
+    //method untuk menampilkan halaman edit
+
+    public function edit($id){
+        //cari data student berdasarkan id
+        $student = Student::find($id);//select * FROM students WHERE id = $id
+
+        return view('admin.contents.student.edit', [
+            'student' => $student
+        ]);
+    }
+
+    //method untuk menyimpan hasil update
+    public function update($id, Request $request){
+        //cari data student dari id
+        $student = Student::find($id);//select * FROM students WHERE id = $id;
+
+        $request->validate([
+            'name' => 'required',
+            'nim' => 'required|numeric',
+            'major' => 'required',
+            'class' => 'required',
+            ]);
+
+            //simpan perubahan 
+            $student->update([
+                'name' => $request->name,
+                'nim' => $request->nim,
+                'major' => $request->major,
+                'class' => $request->class,
+            ]);
+            
+            //kembalikan kehalaman student
+            return redirect('admin/student/')->with('message', 'Data student berhasil diedit');
+    }
+
+    //method untuk menghapus student
+    public function destroy($id){
+        $student = Student::find($id);//select * FROM students WHERE id = $id;
+
+    //hapus student
+    $student->delete();
+
+     //kembalikan kehalaman student
+     return redirect('admin/student/')->with('message', 'Data student berhasil diedit');
     }
 }
--- resources/views/admin/contents/student/index.blade.php
+++ resources/views/admin/contents/student/index.blade.php
@@ -13,6 +13,9 @@
   <section class="section">
     <div class="card">
         <div class="card-body">
+          <a href="/admin/student/create" class="btn btn-primary my-3">Student</a>
+
+          
             <table class="table">
                 <tr>
                     <th>No</th>
@@ -24,14 +27,18 @@
                 </tr>
                 @foreach($students as $student)
                 <tr>
-                    <td>1</td>
+                    <td>{{$student->id}}</td>
                     <td>{{$student->name}}</td>
                     <td>{{$student->nim}}</td>
                     <td>{{$student->class}}</td>
                     <td>{{$student->major}}</td>
-                    <td>
-                        <a href="#" class="btn btn-warning">Edit</a>
-                        <a href="#" class="btn btn-danger">Hapus</a>
+                    <td class="d-flex">
+                        <a href="/admin/student/edit/{{ $student->id }}" class="btn btn-warning me-2">Edit</a>
+                        <form action="/admin/student/delete/{{ $student->id }}" method="post">
+                          @method('DELETE')
+                          @csrf
+                          <button class="btn btn-danger" type="submit" onclick="return confirm('Apakah Anda Yakin?')">Hapus</button>
+                        </form>
                     </td>
                 </tr>
                 @endforeach
